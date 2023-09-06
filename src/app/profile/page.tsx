e708bdbd2ec45c36b6c98b47d5a5d8f05c69563b
anoopraju31 <anoopraju31@gmail.com>
2023-09-06 20:46:02 +0530

    Profile Page: styled

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { CampaignCard } from '../components'
 
 const ProfilePage = () => {
-	return <div>ProfilePage</div>
+	return (
+		<main className='my-8 md:md-14'>
+			<h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>
+				{' '}
+				All Campaigns by the user{' '}
+			</h1>
+
+			{/* All Campaigns */}
+			<section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-[20px] gap-[26px]'>
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+				<CampaignCard />
+			</section>
+		</main>
+	)
 }
 
 export default ProfilePage
