3bd04d45ac9a9f2b7f359c6b7fabf6259a7487c0
anoopraju31 <anoopraju31@gmail.com>
2023-09-06 20:22:36 +0530

    Home Page: heading added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from 'next/image'
 
 export default function Home() {
-	return <main>Home Page</main>
+	return (
+		<main className='my-8 md:md-14'>
+			<h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>
+				{' '}
+				All Campaigns{' '}
+			</h1>
+
+			{/* All Campaigns */}
+			<section className='flex flex-wrap mt-[20px] gap-[26px]'></section>
+		</main>
+	)
 }
