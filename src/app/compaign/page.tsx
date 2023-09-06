9a02799d3b13be481109d0f4020ce94ee94da3c7
anoopraju31 <anoopraju31@gmail.com>
2023-09-06 18:05:29 +0530

    Stat Box Component - styled

--- src/app/compaign/page.tsx
+++ src/app/compaign/page.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 const CompaignDetailsPage = () => {
 	return (
 		<main className='w-full min-h-screen py-10 bg-[#1c1c24]'>
-			<div className='w-full max-w-7xl mx-auto flex md:flex-row flex-col mt-10 gap-[30px]'>
-				{/* Images */}
+			<div className='w-full max-w-7xl px-4 md:px-6 lg:px-8 xl:px-0 mx-auto flex md:flex-row flex-col mt-10 gap-[30px]'>
 				<div className='flex-1 flex-col'>
+					{/* Images */}
 					<Image
 						src='/premium_photo.avif'
 						alt='campaign'
@@ -15,20 +15,22 @@ const CompaignDetailsPage = () => {
 						height={400}
 						className='w-full h-[410px] object-cover rounded-xl'
 					/>
-					<div className='relative w-full h-[5px] bg-[#3a3a43] mt-2'>
+					{/* Progress */}
+					<div className='relative w-full h-[5px] bg-zinc-500 rounded mt-2'>
 						<div
-							className='absolute h-full bg-[#4acd8d]'
+							className='absolute h-full bg-green-500 rounded'
 							style={{
 								width: `10%`,
 								maxWidth: '100%',
 							}}></div>
 					</div>
 				</div>
+
 				{/* Stats */}
-				<div className='flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]'>
-					<StatBox />
-					<StatBox />
-					<StatBox />
+				<div className='flex md:w-[300px] w-full flex-wrap justify-between gap-[30px]'>
+					<StatBox title='Days Left' value={120} />
+					<StatBox title='Raise of $1600' value={100} />
+					<StatBox title='Total Backers' value={13} />
 				</div>
 			</div>
 		</main>
--- src/app/components/StatBox.tsx
+++ src/app/components/StatBox.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const StatBox = () => {
-	return <div>StatBox</div>
+type StatBoxProps = {
+	title: string
+	value: number
+}
+
+const StatBox = ({ title, value }: StatBoxProps) => {
+	return (
+		<div className='w-full flex flex-col items-center '>
+			<h4 className='font-epilogue font-bold text-2xl text-white p-3 bg-[#1c1c24] rounded-t-[10px] w-full text-center truncate'>
+				{title}
+			</h4>
+			<p className='font-epilogue font-normal text-[16px] text-white bg-zinc-500 rounded-b-[10px] px-3 py-2 w-full rouned-b-[10px] text-center'>
+				{value}
+			</p>
+		</div>
+	)
 }
 
 export default StatBox
