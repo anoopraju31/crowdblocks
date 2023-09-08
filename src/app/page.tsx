2bea831d8e299191f7775113bd07189dd229d1ed
anoopraju31 <anoopraju31@gmail.com>
2023-09-08 19:07:10 +0530

    HardHat added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { CampaignCard } from './components'
 export default function Home() {
 	return (
 		<main className='my-8 md:md-14'>
-			<h1 className='font-epilogue font-semibold text-[18px] text-white text-left'></h1>
+			<h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>
+				All Campaigns
+			</h1>
 
 			{/* All Campaigns */}
 			<section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-[20px] gap-[26px]'>
