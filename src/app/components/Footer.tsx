e76dae2360953c945a51a6fcdf13e12b5048031f
anoopraju31 <anoopraju31@gmail.com>
2023-09-09 13:03:46 +0530

    Footer Logo added

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,23 +5,17 @@ import {
 	FaInstagram,
 	FaLinkedinIn,
 } from 'react-icons/fa'
+import { BiLogoBitcoin } from 'react-icons/bi'
 
 const Footer = () => {
 	return (
 		<footer className='text-[#808191]'>
 			<div className='container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col'>
 				<a className='flex title-font font-medium items-center md:justify-start justify-center text-white'>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						fill='none'
-						stroke='currentColor'
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						strokeWidth='2'
-						className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-						viewBox='0 0 24 24'>
-						<path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
-					</svg>
+					<div className='text-green-500'>
+						<BiLogoBitcoin size={30} />
+					</div>
+
 					<span className='ml-3 text-xl'>Crowdblocks</span>
 				</a>
 				<p className='text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4'>
