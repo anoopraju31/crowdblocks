21edc16edfb6bdba7c909ff73c7a928651d78fac
anoopraju31 <anoopraju31@gmail.com>
2023-09-11 19:37:03 +0530

    Abi change

--- src/abis/crowdFunding.ts
+++ src/abis/crowdFunding.ts
@@ -1,11 +1,177 @@
 export const CrowdFundingABI = [
 	{
+		anonymous: false,
 		inputs: [
 			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'id',
+				type: 'uint256',
+			},
+			{
+				indexed: false,
 				internalType: 'address',
-				name: '_owner',
+				name: 'organizer',
 				type: 'address',
 			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'title',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'collectedAmount',
+				type: 'uint256',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'targetAmount',
+				type: 'uint256',
+			},
+		],
+		name: 'CampaignComplete',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'id',
+				type: 'uint256',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'title',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'organizer',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'target',
+				type: 'uint256',
+			},
+		],
+		name: 'CampaignCreated',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: '_id',
+				type: 'uint256',
+			},
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'donor',
+				type: 'address',
+			},
+		],
+		name: 'DonatedToCampaign',
+		type: 'event',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: false,
+				internalType: 'address',
+				name: 'walletAddress',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'name',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'emailId',
+				type: 'string',
+			},
+		],
+		name: 'OrganizerCreated',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		name: 'campaigns',
+		outputs: [
+			{
+				internalType: 'address',
+				name: 'organizer',
+				type: 'address',
+			},
+			{
+				internalType: 'string',
+				name: 'title',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'description',
+				type: 'string',
+			},
+			{
+				internalType: 'uint256',
+				name: 'startDate',
+				type: 'uint256',
+			},
+			{
+				internalType: 'uint256',
+				name: 'deadline',
+				type: 'uint256',
+			},
+			{
+				internalType: 'uint256',
+				name: 'targetAmount',
+				type: 'uint256',
+			},
+			{
+				internalType: 'uint256',
+				name: 'collectedAmount',
+				type: 'uint256',
+			},
+			{
+				internalType: 'bool',
+				name: 'isCompleted',
+				type: 'bool',
+			},
+			{
+				internalType: 'bool',
+				name: 'isValid',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
 			{
 				internalType: 'string',
 				name: '_title',
@@ -17,29 +183,56 @@ export const CrowdFundingABI = [
 				type: 'string',
 			},
 			{
+				internalType: 'string[]',
+				name: '_images',
+				type: 'string[]',
+			},
+			{
+				internalType: 'uint256',
+				name: '_deadline',
+				type: 'uint256',
+			},
+			{
 				internalType: 'uint256',
 				name: '_target',
 				type: 'uint256',
 			},
+		],
+		name: 'createCampaign',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
 			{
-				internalType: 'uint256',
-				name: '_deadLine',
-				type: 'uint256',
+				internalType: 'string',
+				name: '_name',
+				type: 'string',
 			},
 			{
 				internalType: 'string',
-				name: '_image',
+				name: '_contact',
 				type: 'string',
 			},
-		],
-		name: 'createCampaign',
-		outputs: [
+			{
+				internalType: 'string',
+				name: '_emailId',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: '_profile',
+				type: 'string',
+			},
 			{
 				internalType: 'uint256',
-				name: '',
+				name: '_phone',
 				type: 'uint256',
 			},
 		],
+		name: 'createOrganizer',
+		outputs: [],
 		stateMutability: 'nonpayable',
 		type: 'function',
 	},
@@ -57,49 +250,13 @@ export const CrowdFundingABI = [
 		type: 'function',
 	},
 	{
-		inputs: [
-			{
-				internalType: 'uint256',
-				name: '',
-				type: 'uint256',
-			},
-		],
-		name: 'campaigns',
+		inputs: [],
+		name: 'getAllActiveCampaigns',
 		outputs: [
 			{
-				internalType: 'address',
-				name: 'owner',
-				type: 'address',
-			},
-			{
-				internalType: 'string',
-				name: 'title',
-				type: 'string',
-			},
-			{
-				internalType: 'string',
-				name: 'description',
-				type: 'string',
-			},
-			{
-				internalType: 'uint256',
-				name: 'target',
-				type: 'uint256',
-			},
-			{
-				internalType: 'uint256',
-				name: 'deadLine',
-				type: 'uint256',
-			},
-			{
-				internalType: 'uint256',
-				name: 'amountCollected',
-				type: 'uint256',
-			},
-			{
-				internalType: 'string',
-				name: 'image',
-				type: 'string',
+				internalType: 'uint256[]',
+				name: '',
+				type: 'uint256[]',
 			},
 		],
 		stateMutability: 'view',
@@ -107,59 +264,12 @@ export const CrowdFundingABI = [
 	},
 	{
 		inputs: [],
-		name: 'getCampaigns',
+		name: 'getAllCompletedCampaigns',
 		outputs: [
 			{
-				components: [
-					{
-						internalType: 'address',
-						name: 'owner',
-						type: 'address',
-					},
-					{
-						internalType: 'string',
-						name: 'title',
-						type: 'string',
-					},
-					{
-						internalType: 'string',
-						name: 'description',
-						type: 'string',
-					},
-					{
-						internalType: 'uint256',
-						name: 'target',
-						type: 'uint256',
-					},
-					{
-						internalType: 'uint256',
-						name: 'deadLine',
-						type: 'uint256',
-					},
-					{
-						internalType: 'uint256',
-						name: 'amountCollected',
-						type: 'uint256',
-					},
-					{
-						internalType: 'string',
-						name: 'image',
-						type: 'string',
-					},
-					{
-						internalType: 'address[]',
-						name: 'donators',
-						type: 'address[]',
-					},
-					{
-						internalType: 'uint256[]',
-						name: 'donations',
-						type: 'uint256[]',
-					},
-				],
-				internalType: 'struct CrowdFunding.Campaign[]',
+				internalType: 'uint256[]',
 				name: '',
-				type: 'tuple[]',
+				type: 'uint256[]',
 			},
 		],
 		stateMutability: 'view',
@@ -173,17 +283,138 @@ export const CrowdFundingABI = [
 				type: 'uint256',
 			},
 		],
-		name: 'getDonators',
+		name: 'getCampaignImage',
 		outputs: [
 			{
-				internalType: 'address[]',
+				internalType: 'string',
 				name: '',
-				type: 'address[]',
+				type: 'string',
 			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_id',
+				type: 'uint256',
+			},
+		],
+		name: 'getCampaignImages',
+		outputs: [
+			{
+				internalType: 'string[]',
+				name: '',
+				type: 'string[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'getContractBalance',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_address',
+				type: 'address',
+			},
+		],
+		name: 'getIdofCampaignsOrganizedByOrganizer',
+		outputs: [
 			{
 				internalType: 'uint256[]',
 				name: '',
 				type: 'uint256[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'getNumberOfClosedCampaigns',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+		],
+		name: 'getUserContributions',
+		outputs: [
+			{
+				components: [
+					{
+						internalType: 'uint256',
+						name: 'campaignId',
+						type: 'uint256',
+					},
+					{
+						internalType: 'uint256',
+						name: 'contribution',
+						type: 'uint256',
+					},
+					{
+						internalType: 'uint256',
+						name: 'date',
+						type: 'uint256',
+					},
+				],
+				internalType: 'struct CrowdBlocks.UserContribution[]',
+				name: '',
+				type: 'tuple[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'isOrganizer',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'numberOfActiveCampaigns',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
 			},
 		],
 		stateMutability: 'view',
@@ -197,6 +428,79 @@ export const CrowdFundingABI = [
 				internalType: 'uint256',
 				name: '',
 				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'organizers',
+		outputs: [
+			{
+				internalType: 'address',
+				name: 'walletAddress',
+				type: 'address',
+			},
+			{
+				internalType: 'string',
+				name: 'name',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'contact',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'emailId',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'profile',
+				type: 'string',
+			},
+			{
+				internalType: 'uint256',
+				name: 'phone',
+				type: 'uint256',
+			},
+			{
+				internalType: 'bool',
+				name: 'isValid',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		name: 'users',
+		outputs: [
+			{
+				internalType: 'address',
+				name: 'walletAddress',
+				type: 'address',
+			},
+			{
+				internalType: 'bool',
+				name: 'isValid',
+				type: 'bool',
 			},
 		],
 		stateMutability: 'view',
