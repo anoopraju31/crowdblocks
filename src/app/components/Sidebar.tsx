7ea1617634594ea74fe58f5ab1ff992d7079d3c6
anoopraju31 <anoopraju31@gmail.com>
2023-09-11 19:36:37 +0530

    Sidebar component: links change

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -2,9 +2,12 @@
 import { navlinks } from '@/constants'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import type { IconType } from 'react-icons'
 import React from 'react'
-import { BiLogoBitcoin } from 'react-icons/bi'
+import { BiLogoBitcoin, BiUserPlus } from 'react-icons/bi'
+import { MdCreate } from 'react-icons/md'
+import { AiFillHome } from 'react-icons/ai'
+import { IoPersonCircleOutline } from 'react-icons/io5'
+import type { IconType } from 'react-icons'
 
 type Icon = {
 	styles?: string
@@ -41,9 +44,30 @@ const Sidebar = () => {
 
 			<div className='flex-1 flex  flex-col justify-between items-center bg-[#1c1c24] rounded-xl px-2 py-4 mt-12'>
 				<div className='flex flex-col justify-center items-center gap-3'>
-					{navlinks.map((link) => (
-						<Icon key={link.name} {...link} isActive={pathname == link.link} />
-					))}
+					<Icon
+						key='Home'
+						icon={AiFillHome}
+						link='/'
+						isActive={pathname == '/'}
+					/>
+					<Icon
+						key='Create Campaign'
+						icon={MdCreate}
+						link='/create-campaign'
+						isActive={pathname == '/create-campaign'}
+					/>
+					<Icon
+						key='Create Organizer'
+						icon={BiUserPlus}
+						link='/'
+						isActive={pathname == '/create-organizer'}
+					/>
+					<Icon
+						key='Profile'
+						icon={IoPersonCircleOutline}
+						link='/profile'
+						isActive={pathname == '/profile'}
+					/>
 				</div>
 			</div>
 		</div>
