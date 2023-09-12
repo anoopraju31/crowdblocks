2adea7071ac6ea0f21c43e43d6bf2a09dcd7e3b3
anoopraju31 <anoopraju31@gmail.com>
2023-09-13 02:54:31 +0530

    Active Campaign Section static generation

--- src/app/components/ActiveCampagins.tsx
+++ src/app/components/ActiveCampagins.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { CampaignCard } from '.'
+import { CrowdFundingABI } from '@/abis/crowdFunding'
+import { AlchemyProvider } from 'ethers'
+import { ethers } from 'ethers'
 
-const ActiveCampagins = () => {
+export const revalidate = 3600
+
+const ActiveCampagins = async () => {
+	const provider = new AlchemyProvider(
+		'sepolia',
+		process.env.NEXT_PUBLIC_ALCHEMY_ID,
+	)
+	const crowdBlocksContract = new ethers.Contract(
+		'0x4d0b4A2014e64d76CcF0F2E1898bAeba440F7C02',
+		CrowdFundingABI,
+		provider,
+	)
+
+	const campaigns = await crowdBlocksContract.getAllActiveCampaigns()
+
 	return (
 		<section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-[20px] gap-[26px]'>
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
-			<CampaignCard />
+			{campaigns.map((campaignId: BigInt) => (
+				<CampaignCard key={Number(campaignId)} />
+			))}
 		</section>
 	)
 }
