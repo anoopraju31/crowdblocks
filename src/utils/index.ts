2a3378f8047705d7af42b7ea4f9b4e8ec2d93d71
anoopraju31 <anoopraju31@gmail.com>
2023-09-15 16:08:31 +0530

    Campaigns Page initials

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,18 @@
+import { CrowdFundingABI } from '@/abis/crowdFunding'
+import { AlchemyProvider, ethers } from 'ethers'
+
 export const getNumberOfDaysLeft = (timestamp: number): number => {
 	let timestampPresent = Math.floor(new Date().getTime() / 1000.0)
 
 	return Math.ceil((timestamp - timestampPresent) / (60 * 60 * 24))
 }
+
+const provider = new AlchemyProvider(
+	'sepolia',
+	process.env.NEXT_PUBLIC_ALCHEMY_ID,
+)
+export const crowdBlocksContract = new ethers.Contract(
+	'0x4d0b4A2014e64d76CcF0F2E1898bAeba440F7C02',
+	CrowdFundingABI,
+	provider,
+)
