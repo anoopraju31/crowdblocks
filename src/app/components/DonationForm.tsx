a64900bfdb05d27f45e7bf75395922a20f7659d5
anoopraju31 <anoopraju31@gmail.com>
2023-09-15 18:51:52 +0530

    campign dontaion form component donation functionality added

--- src/app/components/DonationForm.tsx
+++ src/app/components/DonationForm.tsx
@@ -4,6 +4,7 @@ import { Button, FormField } from '.'
 import { useAccount, useContractWrite } from 'wagmi'
 import { CrowdFundingABI } from '@/abis/crowdFunding'
 import { parseEther } from 'ethers'
+import ReactLoading from 'react-loading'
 
 type DonationFormType = {
 	campaignId: number
@@ -12,7 +13,7 @@ type DonationFormType = {
 const DonationForm = ({ campaignId }: DonationFormType) => {
 	const [amount, setAmount] = useState<number | string>('')
 	const [isDisabled, setIsDisabled] = useState(false)
-	const { address, isConnected } = useAccount()
+	const { isConnected } = useAccount()
 	const { isLoading, isSuccess, write } = useContractWrite({
 		address: '0x4d0b4A2014e64d76CcF0F2E1898bAeba440F7C02',
 		abi: CrowdFundingABI,
@@ -28,9 +29,17 @@ const DonationForm = ({ campaignId }: DonationFormType) => {
 			args: [campaignId],
 			value: parseEther(String(amount)),
 		})
+
+		setAmount('')
 	}
 	return (
-		<div className='mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]'>
+		<div className='relative mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]'>
+			{isLoading && (
+				<div className='w-full h-full bg-[#1c1c24]/60 rounded-[10px] absolute top-0 left-0 right-0 flex justify-center items-center'>
+					<p className='text-white text-xl'> Loading </p>
+					<ReactLoading type='bubbles' color='#fff' />
+				</div>
+			)}
 			<p className='font-epilogue fount-medium text-[20px] pt-1 leading-[30px] text-center text-[#808191]'>
 				Fund the campaign
 			</p>
