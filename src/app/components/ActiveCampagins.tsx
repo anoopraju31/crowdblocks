f72eb7ed4b0d9602fe76a87b471506c420919ec1
anoopraju31 <anoopraju31@gmail.com>
2023-09-18 13:27:21 +0530

    Active Campaign Component: display message if no active campaigns available

--- src/app/components/ActiveCampagins.tsx
+++ src/app/components/ActiveCampagins.tsx
@@ -22,12 +22,19 @@ const ActiveCampagins = async () => {
 
 	return (
 		<section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-[20px] gap-[26px]'>
-			{campaigns.map((campaignId: BigInt) => (
-				<CampaignCard
-					key={Number(campaignId)}
-					campaignId={Number(campaignId)}
-				/>
-			))}
+			{campaigns.length === 0 ? (
+				<h1 className='w-full h-full col-span-12 underline text-white text-center'>
+					{' '}
+					No Active Campaigns available now!{' '}
+				</h1>
+			) : (
+				campaigns.map((campaignId: BigInt) => (
+					<CampaignCard
+						key={Number(campaignId)}
+						campaignId={Number(campaignId)}
+					/>
+				))
+			)}
 		</section>
 	)
 }
