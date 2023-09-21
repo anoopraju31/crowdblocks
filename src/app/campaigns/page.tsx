7fe99055ffed6d2f7547028a34ac5b421b236a93
anoopraju31 <anoopraju31@gmail.com>
2023-09-21 17:16:29 +0530

    Dropdown Component - initial

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Dropdown } from '../components'
 
-const page = () => {
-	return <div>page</div>
+const CampaignsPage = () => {
+	return (
+		<main className='my-8 md:md-14'>
+			<div className='flex justify-between items-center'>
+				<h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>
+					All Campaigns
+				</h1>
+				<div className='flex gap-4'>
+					<Dropdown />
+					<Dropdown />
+				</div>
+			</div>
+			CampaignsPage
+		</main>
+	)
 }
 
-export default page
+export default CampaignsPage
