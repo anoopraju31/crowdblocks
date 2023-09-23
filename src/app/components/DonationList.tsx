fe040699695d71f22ebd8af6dc44da89f366064f
anoopraju31 <anoopraju31@gmail.com>
2023-09-23 14:34:22 +0530

    Donation List Compoent - styled

--- src/app/components/DonationList.tsx
+++ src/app/components/DonationList.tsx
@@ -1,7 +1,317 @@
 import React from 'react'
 
 const DonationList = () => {
-	return <div>DonationList</div>
+	return (
+		<div className='mt-[20px] relative overflow-x-auto shadow-md sm:rounded-lg'>
+			<table className='w-full text-sm text-left text-gray-400'>
+				<thead className='text-xs uppercase bg-[#1c1c24] text-white'>
+					<tr>
+						<th className='p-4' scope='col'>
+							{' '}
+							Address{' '}
+						</th>
+						<th className='p-4' scope='col'>
+							{' '}
+							Donations{' '}
+						</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+					<tr className='border-b  text-white border-gray-700 hover:bg-zinc-500'>
+						<td className='p-4'>0x19EA0f475B7653Ec108B62D363bcD2dAC3e937e6</td>
+
+						<td className='p-4'>1 eth</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default DonationList
